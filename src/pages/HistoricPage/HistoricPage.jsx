1c5a9c042b695facb380a36209743b39374a0d50
levymc <levymcruz@gmail.com>
2023-06-02 15:23:05 -0300

    campo de busca

--- src/pages/HistoricPage/HistoricPage.jsx
+++ src/pages/HistoricPage/HistoricPage.jsx
@@ -3,10 +3,12 @@ import axios from 'axios';
 import React, { useState, useEffect } from "react";
 import BtnHome from '../../components/BtnHome';
 import Logo from '../../components/Logo';
+import Swal from 'sweetalert2';
 import DataTable from 'react-data-table-component';
 
 export default function HistoricPage() {
     const [data, setData] = useState([]);
+    const [searchText, setSearchText] = useState('');
 
     useEffect(() => {
         axios.get("http://192.168.0.152:4000/api/historico")
@@ -30,22 +32,26 @@ export default function HistoricPage() {
             sortable: true,
         },
     ];
-    
-      
-    const dados = data.map((values, i) => ({
-        data: values.data,
-        oc: values.oc,
-    }));
+
+    const handleSearch = e => {
+        setSearchText(e.target.value);
+    };
+
+    const filteredData = data.filter(item =>
+        item.oc.toString().includes(searchText) ||
+        item.data.includes(searchText)
+    );
 
     return (
         <ContainerPage>
             <BtnHome />
             <Logo />
             <ContainerTable>
+                <section><input type="text" value={searchText} onChange={handleSearch} placeholder="Buscar..." /></section> 
                 <DataTable
                     title="Histórico de OCs"
                     columns={columns}
-                    data={dados}
+                    data={filteredData}
                 />
             </ContainerTable>
         </ContainerPage>
@@ -60,9 +66,19 @@ const ContainerPage = styled.div`
     align-items: center;
     flex-direction: column;
     gap: 2rem;
+    
 `;
 
 const ContainerTable = styled.div`
     width:50%;
     margin: auto;
+    position: relative;
+    height: 50rem;
+    overflow-y: auto;
+    section{
+        position: absolute;
+        top: 0;
+        right: 20px;
+        z-index: 99;
+    }
 `;
